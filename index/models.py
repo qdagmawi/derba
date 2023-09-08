e725fbd3dddffa257a5c65755f465d744a02641f
abdikawter <39310344+abdikawter@users.noreply.github.com>
2023-09-08 15:14:34 +0300

    Add files via upload
    created class for model

--- index/models.py
+++ index/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Signup(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    password_confirm = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+    def name(self):
+        return self.first_name
+
+    objects = models.Manager()
+
