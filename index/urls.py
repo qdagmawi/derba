6e8e9a37a51126a887129256ab006d29efc9be24
abdikawter <39310344+abdikawter@users.noreply.github.com>
2023-09-08 15:18:36 +0300

    Add files via upload

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'car'
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.log, name='login'),
+    path('forgot/', views.forgot, name='forgot'),
+    path('index/', views.index, name='index'),
+    path('logout/', views.user_logout, name='logout'),
 ]
