891fb96b461119ed5fca70e483c4c40561eb2c6d
abdikawter <39310344+abdikawter@users.noreply.github.com>
2023-10-06 15:33:44 +0300

    Update urls.py
    added new path for the detail page.

--- car/urls.py
+++ car/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'car'
 
@@ -10,4 +12,8 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('logout/', views.user_logout, name='logout'),
     path('post/', views.post_car, name='post'),
+    path('detail/', views.detail, name='detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
