134307968d80ae77f2564e7e42c2f1b9f8c9707b
Alazar4 <144335408+Alazar4@users.noreply.github.com>
2023-10-09 09:57:48 +0300

    Update tests.py

--- car/tests.py
+++ car/tests.py
@@ -27,3 +27,25 @@ class CarAppTestCase(TestCase):
             description='This is a test car',
             phone_number='9876543210'
         )
+
+    def test_post_car(self):
+        # Log in the user first
+        self.client.login(email='test@example.com', password='testpassword')
+
+        response = self.client.post(reverse('car:post_car'), {
+            'model': 'New Car',
+            'price': 15000,
+            'car_make': 'New Make',
+            'body_type': 'SUV',
+            'transmission': 'Manual',
+            'year': '2023',
+            'color': 'Red',
+            'description': 'This is a new test car',
+            'phone_number': '1231231234'
+        })
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_logout(self):
+        response = self.client.get(reverse('car:user_logout'))
+        self.assertEqual(response.status_code, 302)
