21cbead3ccd01b685f48f59baf1ff0a053b80fa7
abdikawter <39310344+abdikawter@users.noreply.github.com>
2023-10-09 09:51:18 +0300

    Update tests.py

--- car/tests.py
+++ car/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Signup, Post
 
-# Create your tests here.
+class CarAppTestCase(TestCase):
+
+    def setUp(self):
+        # Create a test user
+        self.user = Signup.objects.create(
+            first_name='Test',
+            last_name='User',
+            email='test@example.com',
+            password='testpassword',
+            phone_number='1234567890'
+        )
+
+        # Create some test cars
+        self.car = Post.objects.create(
+            user=self.user,
+            model='Test Car',
+            price=10000,
+            car_make='Test Make',
+            body_type='Sedan',
+            transmission='Automatic',
+            year='2022',
+            color='Blue',
+            description='This is a test car',
+            phone_number='9876543210'
+        )
